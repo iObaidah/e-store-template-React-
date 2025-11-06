6ad15d18dc8785865f33007b6983baaee5d03c4c
Obaidah <obaidah525@gmail.com>
2025-11-06 09:37:32 +0300

    SearchBar Component Created.

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import './SearchBar.css';
+
+function SearchBar() {
+    const [searchTerm, setSearchTerm] = useState('')
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
