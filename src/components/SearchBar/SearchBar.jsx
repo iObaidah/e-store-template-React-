8c8705f481b39fd61633d00553b53268c30c2a8d
Obaidah <obaidah525@gmail.com>
2025-11-07 11:24:09 +0300

    SearchBar Component Error -- Not pushing

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -3,10 +3,27 @@ import './SearchBar.css';
 
 function SearchBar() {
     const [searchTerm, setSearchTerm] = useState('')
+    const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
 
     return (
-        <div>
-            
+        <div className="search-bar">
+            <div className="search-input">
+                <div className="search-field">
+                    <label htmlFor="search-term">What do you want?</label>
+                    <input
+                        id="search-term"
+                        type="text"
+                        placeholder="Search products..."
+                        value={searchTerm}
+                        onChange={handleSearchChange}
+                    />
+                </div>
+                <button className="search-button">
+                    Search
+                </button>
+            </div>
         </div>
-    )
+    );
 }
