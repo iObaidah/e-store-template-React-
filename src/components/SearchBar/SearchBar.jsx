46bd2b180546831249a4a84d607b10bbfa132a7f
Obaidah <obaidah525@gmail.com>
2025-11-07 12:02:20 +0300

    SearchBar Fix

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,14 +2,23 @@ import React, { useState } from "react";
 import './SearchBar.css';
 
 function SearchBar() {
-    const [searchTerm, setSearchTerm] = useState('')
+    const [searchTerm, setSearchTerm] = useState('');
+    const [location, setLocation] = useState('');
+    const [sortBy, setSortBy] = useState('best_match');
+
     const handleSearchChange = (event) => {
         setSearchTerm(event.target.value);
+    };
+    const handleLocationChange = (event) => {
+        setLocation(event.target.value);
+    }
+    const handleSortClick = (sortOption) => {
+        setSortBy(sortOption);
     };
 
     return (
         <div className="search-bar">
-            <div className="search-input">
+            <div className="search-inputs">
                 <div className="search-field">
                     <label htmlFor="search-term">What do you want?</label>
                     <input
@@ -20,10 +29,30 @@ function SearchBar() {
                         onChange={handleSearchChange}
                     />
                 </div>
+                <div className="search-field">
+                    <label htmlFor="location">Where?</label>
+                    <input
+                        id="location"
+                        type="text"
+                        placeholder="Location..."
+                        value={location}             // Connect to location
+                        onChange={handleLocationChange} // Use location handler
+                    />
+                </div>
                 <button className="search-button">
                     Search
+                </button>
+            </div>
+            <div className="sorting-options">
+                <button
+                    className={`sort-option ${sortBy === 'best_match' ? 'active' : ''}`}
+                    onClick={() => handleSortClick('best_match')}
+                >
+                    Best Match
                 </button>
             </div>
         </div>
     );
 }
+
+export default SearchBar;
